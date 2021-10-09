5bd1b8571d2de197887f9a96840a457c21ca9bb2
LeBronLiHD <lebronlihd@outlook.com>
2021-10-10 01:18:30 +0800

    factor_pca_ica_euclidean_tsne done!

--- factor_pca_ica_euclidean_tsne.py
+++ factor_pca_ica_euclidean_tsne.py
@@ -20,10 +20,10 @@ import parameters
 import preprocess
 from sklearn.utils import shuffle
 import single_feature_distribution
+import matplotlib.pyplot as plt
 
 
 def factor_analysis(data, important):
-    model = decomposition.FactorAnalysis(n_components=2)
     data = data.sample(frac=parameters.SAMPLE_RATIO).reset_index(drop=True)
     print("data.shape ->", data.shape)
     print("important[0] ->", important[0])
@@ -38,18 +38,20 @@ def factor_analysis(data, important):
     data_selected = preprocess.data_normalization(data_selected)
     print("data_selected.shape ->", data_selected.shape)
     print("data_selected.columns ->", data_selected.columns)
+    model = decomposition.FactorAnalysis(n_components=parameters.N_COMPONENTS)
     X = model.fit_transform(data_selected.iloc[:, :-1].values)
     pos = pandas.DataFrame()
     pos['X'] = X[:, 0]
     pos['Y'] = X[:, 1]
     target = data.columns[data.shape[1] - 1]
     pos[target] = data_selected[target]
-    axis = pos[pos[target] == 0].plot(kind='scatter', x='X', y='Y', color='blue', label=0)
+    axis = pos[pos[target] == 0].plot(kind='scatter', x='X', y='Y', color='green', label=0)
     pos[pos[target] == 1].plot(kind='scatter', x='X', y='Y', color='red', label=1, ax=axis)
+    plt.title("factor_analysis")
+    plt.show()
 
 
 def PCA(data, important):
-    model = decomposition.FactorAnalysis(n_components=2)
     data = data.sample(frac=parameters.SAMPLE_RATIO).reset_index(drop=True)
     print("data.shape ->", data.shape)
     print("important[0] ->", important[0])
@@ -64,8 +66,8 @@ def PCA(data, important):
     data_selected = preprocess.data_normalization(data_selected)
     print("data_selected.shape ->", data_selected.shape)
     print("data_selected.columns ->", data_selected.columns)
-    pca = decomposition.PCA(n_components=2)
-    X = pca.fit_transform(data_selected.iloc[:, :-1])
+    model = decomposition.PCA(n_components=parameters.N_COMPONENTS)
+    X = model.fit_transform(data_selected.iloc[:, :-1])
     pos = pandas.DataFrame()
     pos['X'] = X[:, 0]
     pos['Y'] = X[:, 1]
@@ -73,7 +75,91 @@ def PCA(data, important):
     pos[target] = data_selected[target]
     axis = pos[pos[target] == 0].plot(kind='scatter', x='X', y='Y', color='blue', label=0)
     pos[pos[target] == 1].plot(kind='scatter', x='X', y='Y', color='red', label=1, ax=axis)
-    print("explained_variance_ratio_ ->", pca.fit(data_selected.iloc[:, :-1].values).explained_variance_ratio_)
+    print("explained_variance_ratio_ ->", model.fit(data_selected.iloc[:, :-1].values).explained_variance_ratio_)
+    plt.title("PCA")
+    plt.show()
+
+
+def FastICA(data, important):
+    data = data.sample(frac=parameters.SAMPLE_RATIO).reset_index(drop=True)
+    print("data.shape ->", data.shape)
+    print("important[0] ->", important[0])
+    important_copy = []
+    for i in range(len(important[0])):
+        important_copy.append(important[0][i])
+    important_copy.append(data.shape[1] - 1)
+    select_col = []
+    for i in range(len(important_copy)):
+        select_col.append(data.columns[important_copy[i]])
+    data_selected = pandas.DataFrame(data, columns=select_col)
+    data_selected = preprocess.data_normalization(data_selected)
+    print("data_selected.shape ->", data_selected.shape)
+    print("data_selected.columns ->", data_selected.columns)
+    model = decomposition.FastICA(n_components=parameters.N_COMPONENTS)
+    X = model.fit_transform(data_selected.iloc[:, :-1])
+    pos = pandas.DataFrame()
+    pos['X'] = X[:, 0]
+    pos['Y'] = X[:, 1]
+    target = data.columns[data.shape[1] - 1]
+    pos[target] = data_selected[target]
+    axis = pos[pos[target] == 0].plot(kind='scatter', x='X', y='Y', color='orange', label=0)
+    pos[pos[target] == 1].plot(kind='scatter', x='X', y='Y', color='red', label=1, ax=axis)
+    plt.title("FastICA")
+    plt.show()
+
+
+def euclidean(data, important):
+    data = data.sample(frac=parameters.SAMPLE_RATIO).reset_index(drop=True)
+    print("data.shape ->", data.shape)
+    print("important[0] ->", important[0])
+    important_copy = []
+    for i in range(len(important[0])):
+        important_copy.append(important[0][i])
+    important_copy.append(data.shape[1] - 1)
+    select_col = []
+    for i in range(len(important_copy)):
+        select_col.append(data.columns[important_copy[i]])
+    data_selected = pandas.DataFrame(data, columns=select_col)
+    data_selected = preprocess.data_normalization(data_selected)
+    print("data_selected.shape ->", data_selected.shape)
+    print("data_selected.columns ->", data_selected.columns)
+    similarities = euclidean_distances(data_selected.iloc[:, :-1].values)
+    model = manifold.MDS(n_components=2, max_iter=3000, eps=1e-9, dissimilarity="precomputed", n_jobs=1)
+    X = model.fit(similarities).embedding_
+    pos = pandas.DataFrame(X, columns=['X', 'Y'])
+    pos['X'] = X[:, 0]
+    pos['Y'] = X[:, 1]
+    target = data.columns[data.shape[1] - 1]
+    pos[target] = data_selected[target]
+    axis = pos[pos[target] == 0].plot(kind='scatter', x='X', y='Y', color='cyan', label=0)
+    pos[pos[target] == 1].plot(kind='scatter', x='X', y='Y', color='red', label=1, ax=axis)
+    plt.title("euclidean_distances")
+    plt.show()
+
+
+def tSNE(data, important):
+    data = data.sample(frac=parameters.SAMPLE_RATIO).reset_index(drop=True)
+    print("data.shape ->", data.shape)
+    print("important[0] ->", important[0])
+    important_copy = []
+    for i in range(len(important[0])):
+        important_copy.append(important[0][i])
+    important_copy.append(data.shape[1] - 1)
+    select_col = []
+    for i in range(len(important_copy)):
+        select_col.append(data.columns[important_copy[i]])
+    data_selected = pandas.DataFrame(data, columns=select_col)
+    data_selected = preprocess.data_normalization(data_selected)
+    print("data_selected.shape ->", data_selected.shape)
+    print("data_selected.columns ->", data_selected.columns)
+    date_embedded = TSNE(n_components=2).fit_transform(data_selected.iloc[:, :-1])
+    pos = pandas.DataFrame(date_embedded, columns=['X', 'Y'])
+    target = data.columns[data.shape[1] - 1]
+    pos[target] = data_selected[target]
+    axis = pos[pos[target] == 0].plot(kind='scatter', x='X', y='Y', color='fuchsia', label=0)
+    pos[pos[target] == 1].plot(kind='scatter', x='X', y='Y', color='red', label=1, ax=axis)
+    plt.title("tSNE")
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -83,7 +169,8 @@ if __name__ == '__main__':
     #     preprocess.data_cleaning(end_off), preprocess.data_cleaning(merge), \
     #     preprocess.data_cleaning(end_off_feature), preprocess.data_cleaning(merge_feature)
     important, important_h = single_feature_distribution.single_feature(end_off, end_off_feature, end_off_target, False)
-    factor_analysis(end_off, important)
-    PCA(end_off, important)
     factor_analysis(end_off, important_h)
     PCA(end_off, important_h)
+    FastICA(end_off, important_h)
+    euclidean(end_off, important_h)
+    tSNE(end_off, important_h)
