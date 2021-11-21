8dc454290737b55e0ac9418a356e5840b6597199
LeBronLiHD <lebronlihd@outlook.com>
2021-11-21 20:36:05 +0800

    3, 3 -> 2, 2

--- j_CAE_binary_classification.py
+++ j_CAE_binary_classification.py
@@ -38,20 +38,20 @@ def TrainCAEModel(x_train, y_train, x_test, y_test):
 
     input_img = Input(shape=(width, height, 1))
 
-    x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+    x = Conv2D(16, (2, 2), activation='relu', padding='same')(input_img)
     x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = Conv2D(8, (2, 2), activation='relu', padding='same')(x)
     x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = Conv2D(8, (2, 2), activation='relu', padding='same')(x)
     encoded = MaxPooling2D((2, 2), padding='same')(x)
 
-    x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+    x = Conv2D(8, (2, 2), activation='relu', padding='same')(encoded)
     x = UpSampling2D((2, 2))(x)
-    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = Conv2D(8, (2, 2), activation='relu', padding='same')(x)
     x = UpSampling2D((2, 2))(x)
-    x = Conv2D(16, (3, 3), activation='relu')(x)
+    x = Conv2D(16, (2, 2), activation='relu')(x)
     x = UpSampling2D((2, 2))(x)
-    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+    decoded = Conv2D(1, (2, 2), activation='sigmoid', padding='same')(x)
 
     autoencoder = Model(input_img, decoded)
     autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
